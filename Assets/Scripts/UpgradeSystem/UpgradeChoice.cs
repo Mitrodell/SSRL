b49fb2b2a9cb73dba63d6d8238f878890eee4f6c
Mitrodell <mitrodell3545@gmail.com>
2026-02-18 09:18:09 +0500

    Icons for upgrades

--- Assets/Scripts/UpgradeSystem/UpgradeChoice.cs
+++ Assets/Scripts/UpgradeSystem/UpgradeChoice.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Reflection;
 using UnityEngine;
 
 public sealed class UpgradeChoice
@@ -8,4 +7,19 @@ public sealed class UpgradeChoice
     public string title;
     public Action<PlayerStats> apply;
     public bool oneTime;
+    public string iconResourcePath;
+
+    private Sprite cachedIcon;
+
+    public Sprite Icon
+    {
+        get
+        {
+            if (cachedIcon != null || string.IsNullOrWhiteSpace(iconResourcePath))
+                return cachedIcon;
+
+            cachedIcon = Resources.Load<Sprite>(iconResourcePath);
+            return cachedIcon;
+        }
+    }
 }
--- Assets/Scripts/UpgradeSystem/UpgradeSystem.cs
+++ Assets/Scripts/UpgradeSystem/UpgradeSystem.cs
@@ -11,18 +11,21 @@ public static class UpgradeSystem
             title = "+20 Max HP",
             apply = (ps) => ps.AddMaxHp(20f),
             oneTime= false,
+            iconResourcePath = "Upgrades/max_hp_20",
         },
         new UpgradeChoice {
             id = "heal_25",
             title = "Heal +25",
             apply = (ps) => ps.Heal(25f),
-            oneTime=false
+            oneTime=false,
+            iconResourcePath = "Upgrades/heal_25",
         },
         new UpgradeChoice {
             id = "move_speed_10p",
             title = "+10% Move Speed",
             apply = (ps) => ps.AddMoveSpeed(ps.MoveSpeed * 0.10f),
-            oneTime=false
+            oneTime=false,
+            iconResourcePath = "Upgrades/move_speed_10p",
         },
 
         // Global weapon
@@ -30,13 +33,15 @@ public static class UpgradeSystem
             id = "weapon_damage_20p",
             title = "+20% Weapon Damage",
             apply = (ps) => WithCurrentWeapon(ps, w => w.MulDamage(1.20f)),
-            oneTime=false
+            oneTime=false,
+            iconResourcePath = "Upgrades/weapon_damage_20p",
         },
         new UpgradeChoice {
             id = "weapon_firerate_15p",
             title = "+15% Fire Rate",
             apply = (ps) => WithCurrentWeapon(ps, w => w.AddFireRate(w.FireRate * 0.15f)),
-            oneTime=false
+            oneTime=false,
+            iconResourcePath = "Upgrades/weapon_firerate_15p",
         },
 
         // Melee weapon
@@ -44,19 +49,22 @@ public static class UpgradeSystem
             id = "melee_radius_up",
             title = "Melee: +0.4 Radius",
             apply = (ps) => WithWeapon<MeleeSplashWeapon>(ps, w => w.AddRadius(0.4f)),
-            oneTime=false
+            oneTime=false,
+            iconResourcePath = "Upgrades/melee_radius_up",
         },
         new UpgradeChoice {
             id = "melee_angle_up",
             title = "Melee: +20° Arc",
             apply = (ps) => WithWeapon<MeleeSplashWeapon>(ps, w => w.AddAngle(20f)),
-            oneTime=false
+            oneTime=false,
+            iconResourcePath = "Upgrades/melee_angle_up",
         },
         new UpgradeChoice {
             id = "melee_damage_25p",
             title = "Melee: +25% Damage",
             apply = (ps) => WithWeapon<MeleeSplashWeapon>(ps, w => w.MulDamage(1.25f)),
             oneTime=false,
+            iconResourcePath = "Upgrades/melee_damage_25p",
         },
 
         // Gun weapon
@@ -64,13 +72,15 @@ public static class UpgradeSystem
             id = "gun_proj_speed_up",
             title = "Gun: +20% Bullet Speed",
             apply = (ps) => WithWeapon<ProjectileWeapon>(ps, w => w.MulProjectileSpeed(1.20f)),
-            oneTime=false
+            oneTime=false,
+            iconResourcePath = "Upgrades/gun_proj_speed_up",
         },
         new UpgradeChoice {
             id = "gun_pierce_on",
             title = "Gun: Piercing Shots",
             apply = (ps) => WithWeapon<ProjectileWeapon>(ps, w => w.SetPierce(true)),
-            oneTime=true
+            oneTime=true,
+            iconResourcePath = "Upgrades/gun_pierce_on",
         },
 
         //Beam weapon
@@ -78,19 +88,22 @@ public static class UpgradeSystem
             id = "beam_range_up",
             title = "Beam: +25% Range",
             apply = (ps) => WithWeapon<PiercingBeamWeapon>(ps, w => w.MulRange(1.25f)),
-            oneTime=false
+            oneTime=false,
+            iconResourcePath = "Upgrades/beam_range_up",
         },
         new UpgradeChoice {
             id = "beam_hits_up",
             title = "Beam: +2 Targets",
             apply = (ps) => WithWeapon<PiercingBeamWeapon>(ps, w => w.AddMaxHits(2)),
-            oneTime=false
+            oneTime=false,
+            iconResourcePath = "Upgrades/beam_hits_up",
         },
         new UpgradeChoice {
             id = "beam_damage_25p",
             title = "Beam: +25% Damage",
             apply = (ps) => WithWeapon<PiercingBeamWeapon>(ps, w => w.MulDamage(1.25f)),
-            oneTime=false
+            oneTime=false,
+            iconResourcePath = "Upgrades/beam_damage_25p",
         },
     };
 
